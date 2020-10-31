76b98bb61324b763d238b58d30e35daf818030d4
Ferdinand Malcher <ferdinand@malcher.media>
2020-10-31 22:02:33 +0100

    feat: provide own implementation for object path utilities
    heavily inspired by https://github.com/ozum/immutable-path – THANK YOU!
    This also makes the lib finally compatible with a browser. This has been tested in an Angular app, thus resolve #16

--- packages/mixer-connection/src/lib/state/mixer-store.ts
+++ packages/mixer-connection/src/lib/state/mixer-store.ts
@@ -1,18 +1,18 @@
 import { Observable } from 'rxjs';
 import { filter, map, scan, shareReplay } from 'rxjs/operators';
-import * as immutable from 'object-path-immutable';
+import { setObjectPath } from '../utils/object-path';
 
 import { transformStringValue } from '../util';
 
 export class MixerStore {
   readonly state$ = this.rawMessages$.pipe(
-    map(msg => msg.match(/(SETD|SETS)\^([a-zA-Z0-9.]+)\^(.+)/)),
+    map(msg => msg.match(/(SETD|SETS)\^([a-zA-Z0-9.]+)\^(.*)/)),
     filter(e => !!e),
     map(([, , path, value]) => ({
       path: path.split('.').map(transformStringValue),
       value: transformStringValue(value),
     })),
-    scan((acc, { path, value }) => immutable.set(acc, path, value), {}),
+    scan((acc, { path, value }) => setObjectPath(acc, path, value), {}),
     shareReplay(1)
   );
 
