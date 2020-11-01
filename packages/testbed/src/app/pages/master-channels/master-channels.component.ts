dd55aa6b356796b613f0321d30a9e3832dc9db89
Ferdinand Malcher <ferdinand@malcher.media>
2020-11-01 10:29:26 +0100

    feat: PAN: only on Master and AUX channels, add setPan functionality

--- packages/testbed/src/app/pages/master-channels/master-channels.component.ts
+++ packages/testbed/src/app/pages/master-channels/master-channels.component.ts
@@ -13,10 +13,10 @@ export class MasterChannelsComponent implements OnInit {
     { channel: this.master.input(2), label: 'Input 2' },
     { channel: this.master.line(1), label: 'Line 1' },
     { channel: this.master.player(1), label: 'Player 1' },
-    { channel: this.master.aux(2), label: 'AUX 2' },
     { channel: this.master.fx(2), label: 'FX 2' },
     { channel: this.master.sub(3), label: 'Sub group 3' },
-    { channel: this.master.vca(4), label: 'VCA 4' },
+    { channel: this.master.aux(2), label: 'AUX 2', noPan: true },
+    { channel: this.master.vca(4), label: 'VCA 4', noPan: true },
   ];
 
   constructor(private cs: ConnectionService) {}
