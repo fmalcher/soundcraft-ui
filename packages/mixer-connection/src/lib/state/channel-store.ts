e1ebeb777dc86039153a1f4e351657501fc461b5
Ferdinand Malcher <ferdinand@malcher.media>
2022-12-06 17:16:56 +0100

    refactor: channel store can hold any kind of object
    The channel store is not limited to channel objects at all. We might want to store buses or mute groups, too.

--- packages/mixer-connection/src/lib/state/channel-store.ts
+++ packages/mixer-connection/src/lib/state/channel-store.ts
@@ -1,21 +1,16 @@
-import { HwChannel } from '../facade/hw-channel';
-import { FadeableChannel } from '../facade/interfaces';
-
-type StorableChannel = FadeableChannel | HwChannel;
-
 /**
- * Store for channel objects.
- * This is used to cache channel objects so that they don't need to be recreated all the time.
+ * Store for facade objects
+ * This is used to cache channels and others so that they don't need to be recreated all the time.
  * This is just a wrapper around a "Map" object, but we like to keep it abstract.
  */
 export class ChannelStore {
-  private store = new Map<string, StorableChannel>();
+  private store = new Map<string, unknown>();
 
-  get<T extends StorableChannel>(id: string) {
+  get<T>(id: string) {
     return this.store.get(id) as T;
   }
 
-  set(id: string, value: StorableChannel) {
+  set(id: string, value: unknown) {
     this.store.set(id, value);
   }
 }
