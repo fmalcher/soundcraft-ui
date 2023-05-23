e8dea4e39743b61bfc1fca31595c36d088683627
Ferdinand Malcher <ferdinand@malcher.media>
2023-05-23 14:06:07 +0200

    build: use Vite for bundling

--- packages/mixer-connection/jest.config.ts
+++ packages/mixer-connection/jest.config.ts
@@ -1,13 +1,10 @@
 /* eslint-disable */
 export default {
   displayName: 'mixer-connection',
-
-  globals: {},
-  transform: {
-    '^.+\\.[tj]sx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/mixer-connection',
-  testEnvironment: 'node',
   preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/mixer-connection',
 };
