b399ba6ff3a86f3ee85457118d132738da0eb30c
Ferdinand Malcher <ferdinand@malcher.media>
2023-05-23 14:30:59 +0200

    build: add NPM OTP to publish script

--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -23,7 +23,7 @@ function invariant(condition, message) {
 
 // Executing publish script: node path/to/publish.mjs {name} --version {version} --tag {tag}
 // Default "tag" to "next" so we won't publish the "latest" tag by accident.
-const [, , name, version, tag = 'next'] = process.argv;
+const [, , name, version, tag = 'next', otp] = process.argv;
 
 // A simple SemVer validation to validate the version
 const validVersion = /^\d+\.\d+\.\d+(-\w+\.\d+)?/;
@@ -58,4 +58,4 @@ try {
 }
 
 // Execute "npm publish" to publish
-execSync(`npm publish --access public --tag ${tag}`);
+execSync(`npm publish --access public --tag ${tag} --otp ${otp}`);
