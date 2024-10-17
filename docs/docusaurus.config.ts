0b4eba1a48658021c3cf1743f1fa6483c66b05db
Ferdinand Malcher <ferdinand@malcher.media>
2024-10-17 14:10:08 +0200

    first draft for docs

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,101 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'soundcraft-ui-connection',
+  tagline: 'Connection Library for the Soundcraft Ui Series Audio Mixers',
+  // favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://fmalcher.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/doctest/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'fmalcher',
+  projectName: 'doctest',
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
+
+  onBrokenLinks: 'warn',// 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/fmalcher/soundcraft-ui/tree/main/docs/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    navbar: {
+      title: 'soundcraft-ui-connection',
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'docSidebar',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          href: 'https://github.com/fmalcher/soundcraft-ui',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Documentation',
+              to: '/docs',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/fmalcher/soundcraft-ui',
+            },
+          ],
+        },
+      ],
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ docs/src/pages/index.md
@@ -0,0 +1,8 @@
+# soundcraft-ui-connection
+
+**Connection library for the Soundcraft Ui series mixers (Ui12, Ui16 and Ui24R), written in TypeScript/JavaScript**
+
+This library provides a generic connection interface for for Soundcraft Ui mixers.
+It can be used in Node.js or in the browser to control most of the mixer's features without using the built-in web app.
+
+# SERVED FROM DOCS SUBFOLDER
