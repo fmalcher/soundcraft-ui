d4b0397df3a6f5fa1d4e865e29969a8c0a8d51d0
Ferdinand Malcher <ferdinand@malcher.media>
2024-10-17 14:12:40 +0200

    docs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,5 +1,5 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
@@ -11,16 +11,16 @@ const config: Config = {
   url: 'https://fmalcher.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/doctest/',
+  baseUrl: '/soundcraft-ui/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'fmalcher',
-  projectName: 'doctest',
+  projectName: 'soundcraft-ui',
   deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
-  onBrokenLinks: 'warn',// 'throw',
+  onBrokenLinks: 'warn', // 'throw',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
@@ -40,8 +40,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/fmalcher/soundcraft-ui/tree/main/docs/',
+          editUrl: 'https://github.com/fmalcher/soundcraft-ui/tree/main/docs/',
         },
         blog: false,
         theme: {
