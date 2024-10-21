3099b6404d92733ee632c5f7ed3c1b321a52987d
Ferdinand Malcher <ferdinand@malcher.media>
2024-10-21 16:08:26 +0200

    feat: add support for FX parameters

--- docs/docs/usage/notes.md
+++ docs/docs/usage/notes.md
@@ -5,4 +5,5 @@ sidebar_position: 3
 # General Notes
 
 - All channel objects are cached and treated as singletons. If you call `conn.master.input(3)` multiple times, each call returns the exact same object.
-- Input values for fader levels and pan are restricted to the range of `0..1`. All other values are not checked or sanitized in any way! Be sure to call the methods with valid values only.
+- Input values for fader levels and pan are restricted to the range of `0..1`. Most other values are not checked or sanitized in any way! Be sure to call the methods with valid values only.
+- All fader ranges are expressed as values between `0` and `1` internally. This library refers to these as _linear values_. For some parameters, we also publish the values transformed to their physical unit, e.g. `dB` for volume levels. This is useful if values should be displayed to the user.
--- packages/mixer-connection/src/lib/facade/fx-bus.spec.ts
+++ packages/mixer-connection/src/lib/facade/fx-bus.spec.ts
@@ -40,4 +40,69 @@ describe('FX Bus', () => {
     bus.setBpm(60);
     expect(await firstValueFrom(bus.bpm$)).toBe(60);
   });
+
+  describe('params', () => {
+    describe('getParam', () => {
+      it('should not allow params out of range 1..6', () => {
+        expect(() => conn.fx(1).getParam(0)).toThrow();
+        expect(() => conn.fx(2).getParam(7)).toThrow();
+        expect(() => conn.fx(3).getParam(-3)).toThrow();
+        expect(() => conn.fx(4).getParam(9)).toThrow();
+
+        expect(() => conn.fx(4).getParam(3)).not.toThrow();
+      });
+
+      it('should return linear values', async () => {
+        conn.conn.sendMessage('SETD^f.0.par2^0.66');
+        expect(await firstValueFrom(conn.fx(1).getParam(2))).toBe(0.66);
+
+        conn.conn.sendMessage('SETD^f.1.par1^0.123');
+        expect(await firstValueFrom(conn.fx(2).getParam(1))).toBe(0.123);
+
+        conn.conn.sendMessage('SETD^f.2.par4^0.75');
+        expect(await firstValueFrom(conn.fx(3).getParam(4))).toBe(0.75);
+
+        conn.conn.sendMessage('SETD^f.3.par5^1');
+        expect(await firstValueFrom(conn.fx(4).getParam(5))).toBe(1);
+      });
+    });
+
+    describe('setParam', () => {
+      it('should not allow params out of range 1..6', () => {
+        expect(() => conn.fx(1).setParam(0, 0.5)).toThrow();
+        expect(() => conn.fx(2).setParam(7, 0.4)).toThrow();
+        expect(() => conn.fx(3).setParam(-5, 0.6)).toThrow();
+        expect(() => conn.fx(4).setParam(10, 0.3)).toThrow();
+
+        expect(() => conn.fx(4).setParam(3, 0.3)).not.toThrow();
+      });
+
+      it('should set FX params', async () => {
+        conn.fx(1).setParam(6, 0.55);
+        expect(await firstValueFrom(conn.fx(1).getParam(6))).toBe(0.55);
+
+        conn.fx(2).setParam(5, 1);
+        expect(await firstValueFrom(conn.fx(2).getParam(5))).toBe(1);
+
+        conn.fx(3).setParam(4, 0);
+        expect(await firstValueFrom(conn.fx(3).getParam(4))).toBe(0);
+
+        conn.fx(4).setParam(3, 0.112233);
+        expect(await firstValueFrom(conn.fx(4).getParam(3))).toBe(0.112233);
+      });
+
+      it('should send messages to set params', () => {
+        conn.conn.sendMessage = jest.fn();
+
+        conn.fx(1).setParam(1, 0.55);
+        expect(conn.conn.sendMessage).toHaveBeenCalledWith('SETD^f.0.par1^0.55');
+
+        conn.fx(2).setParam(6, 0.1111);
+        expect(conn.conn.sendMessage).toHaveBeenCalledWith('SETD^f.1.par6^0.1111');
+
+        conn.fx(4).setParam(2, 1);
+        expect(conn.conn.sendMessage).toHaveBeenCalledWith('SETD^f.3.par2^1');
+      });
+    });
+  });
 });
