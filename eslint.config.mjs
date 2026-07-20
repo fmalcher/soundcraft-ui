f6d935600f83a30e1f4ce0a62540fe6bbb10c24b
Ferdinand Malcher <ferdinand@malcher.media>
2026-07-20 23:39:50 +0200

    deps: nx migrate 23.1.0 and upgrade to Angular 22

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import nx from '@nx/eslint-plugin';
+
+export default [
+  ...nx.configs['flat/base'],
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: [],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+];
