67435134e3982129ead1cca6a565dea0962aba69
Ferdinand Malcher <ferdinand@malcher.media>
2026-07-21 09:57:45 +0200

    refactor(testbed): use component input binding for route params

--- packages/testbed/src/app/app.config.ts
+++ packages/testbed/src/app/app.config.ts
@@ -1,8 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
-import { TitleStrategy, provideRouter } from '@angular/router';
+import { TitleStrategy, provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { CustomTitleStrategy } from './title-strategy';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), { provide: TitleStrategy, useClass: CustomTitleStrategy }],
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    { provide: TitleStrategy, useClass: CustomTitleStrategy },
+  ],
 };
